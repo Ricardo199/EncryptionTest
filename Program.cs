7992d8e6976dc4a4b81f431abeb1945a51eedd40
austinchima <austinchima515@gmail.com>
2025-09-03 19:14:18 -0400

    Removed an unneccasary comment

--- Program.cs
+++ Program.cs
@@ -13,15 +13,13 @@ namespace EncryptionTest
             string plainText = ReadLine();
             int key = 17;
 
-            // This is the correct way to call the static method.
+            
             string encryptedText = CaesarCipher.Encryption(plainText, key);
 
             // Print the result
-            WriteLine($"Original Text: {plainText}");
             WriteLine();
             WriteLine();
             WriteLine($"Encrypted Text: {encryptedText}");
-            WriteLine();
             WriteLine();
 
             ReadKey();
